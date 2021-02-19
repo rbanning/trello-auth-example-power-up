34c530d26ca53ec62dc0906d52743fa059f78faf
rbanning <rob@myhallpass.com>
2021-02-19 11:57:17 -0800

    added board and card to exploreMembers

--- src/ts/connector.ts
+++ src/ts/connector.ts
@@ -38,5 +38,16 @@ function exploreMembers(t: any) {
   t.member('all')
     .then((member: any) => {
       console.log("DEBUG: exploreMember - Me", {member, canWriteToModel: t.memberCanWriteToModel('card')});
-    })  
+    });
+    
+  t.board('id', 'name', 'members', 'memberships')
+    .then((board: any) => {
+      console.log("DEBUG: exploreMember - Board", {board});
+    });
+
+  t.card('id', 'name', 'members', 'due', 'dueComplete')
+    .then((card: any) => {
+      console.log("DEBUG: exploreMember - Card", {card});
+    });
+
 }
