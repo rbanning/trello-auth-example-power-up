764f03211d678ad41f5ccbb931529580885e8680
rbanning <rob@myhallpass.com>
2021-02-19 11:49:10 -0800

    exploring member - me (see #1)

--- src/ts/connector.ts
+++ src/ts/connector.ts
@@ -5,7 +5,7 @@ console.log("connector", {env});
 
 
 (window as any).TrelloPowerUp.initialize({
-  'board-buttons': (t) => {
+  'board-buttons': (t: any) => {
     return [
       {
         text: 'Meeting Summary',
@@ -13,14 +13,30 @@ console.log("connector", {env});
         callback: meetingSummary 
       }
     ]
+  },
+  'card-detail-badges': (t: any) => {
+    //todo: restrict access to admins... how?
+    return [
+      {
+        text: 'Explore Members/Membership',        
+        callback: exploreMembers
+      }
+    ]
   }
 });
 
 
-function meetingSummary(t) {
+function meetingSummary(t: any) {
   return t.popup({
     title: 'Meeting Summary',
     url: './meeting-summary.html',
     height: 300
   });
+}
+
+function exploreMembers(t: any) {
+  t.member('all')
+    .then((member: any) => {
+      console.log("DEBUG: exploreMember - Me", {member, canWriteToModel: t.memberCanWriteToModel('card')});
+    })  
 }
