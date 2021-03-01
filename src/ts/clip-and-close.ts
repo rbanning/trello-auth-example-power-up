ccdb2735ae27bd79e7cd0d7f8d9502d0fd0a4d87
rbanning <rob@myhallpass.com>
2021-03-01 13:53:34 -0800

    trying to copy to clipboard

--- src/ts/clip-and-close.ts
+++ src/ts/clip-and-close.ts
@@ -18,28 +18,28 @@ t.render(() => {
 
   const copyToClipboard = (element: any) => {
 
-    console.log("copyToClipboard - 0");
-    element.contentEditable = true;
-    console.log("copyToClipboard - 1");
-    element.focus();
-    console.log("copyToClipboard - 2");
-    const range = document.createRange();
-    console.log("copyToClipboard - 3");
-    range.selectNodeContents(element);
-    console.log("copyToClipboard - 4");
-    const sel = window.getSelection();
-    console.log("copyToClipboard - 5");
-    sel.removeAllRanges();
-    console.log("copyToClipboard - 6");
-    sel.addRange(range);
+    // console.log("copyToClipboard - 0");
+    // element.contentEditable = true;
+    // console.log("copyToClipboard - 1");
+    // element.focus();
+    // console.log("copyToClipboard - 2");
+    // const range = document.createRange();
+    // console.log("copyToClipboard - 3");
+    // range.selectNodeContents(element);
+    // console.log("copyToClipboard - 4");
+    // const sel = window.getSelection();
+    // console.log("copyToClipboard - 5");
+    // sel.removeAllRanges();
+    // console.log("copyToClipboard - 6");
+    // sel.addRange(range);
 
-    console.log("DEBUG copyToClipboard", {element, range, sel});
+    // console.log("DEBUG copyToClipboard", {element, range, sel});
 
     //*** CANNOT RUN COPY DUE TO PERMISSION ERROR ***/
-    // element.contentEditable = true;
-    // element.focus();
-    // document.execCommand('selectAll');
-    // document.execCommand('copy');
+    element.contentEditable = true;
+    element.focus();
+    document.execCommand('selectAll');
+    document.execCommand('copy');
     // element.contentEditable = false;
     // getSelection().empty(); //clear
   }
