caf9366ff43b9e11477241c715ca6df462ec307f
Rob Banning <bannjunk@gmail.com>
2021-12-09 11:13:07 -0800

    Add dynamic card badge

--- src/ts/card-badge.ts
+++ src/ts/card-badge.ts
@@ -5,11 +5,19 @@ export namespace CardBadge {
   const processLocationCard = (card: any) => {
     if (card?.coordinates) {
       console.log("BADGE", {name: card.name, card});
+      let count:number = null;
       return {
-        text: `${card.locationName}`,
-        icon: env.logo.white,
-        color: 'sky'
-      };  
+        dynamic: () => {
+          console.log("BADGE", {name: card.name, count});
+          count = (count == null) ? 1000 : (count + 1);
+          return {
+              text: `${count}: ${card.locationName}`,
+              icon: env.logo.white,
+              color: 'sky'
+          }
+        },
+        refresh: 30
+      };
     }
     //else
     return null;
