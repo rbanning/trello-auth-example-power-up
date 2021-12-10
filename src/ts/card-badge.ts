d20c802843bd5732cd9e96b89ab7a23f0479b662
rbanning <rob@myhallpass.com>
2021-12-10 09:40:46 -0800

    no dynamic during debug?

--- src/ts/card-badge.ts
+++ src/ts/card-badge.ts
@@ -1,6 +1,5 @@
 import { TimeService } from "./time.server";
 import { env, trello } from "./_common";
-import { DateHelper } from './date-helper';
 import { ITimeModel } from "./time.model";
 export namespace CardBadge {
 
@@ -9,40 +8,59 @@ export namespace CardBadge {
       console.log("BADGE", {name: card.name, card});
       let timeModel:ITimeModel = null;
 
-      return {
-        dynamic: () => {
-          if (timeModel == null) {
-            const service = new TimeService(t);
-            const {latitude, longitude} = card.coordinates;
-            return service.fetchCurrentTimeFromApi(latitude, longitude)
-              .then((result: ITimeModel) => {
-                //check for error
-                if (!result) { return null; }
+      const service = new TimeService(t);
+      const {latitude, longitude} = card.coordinates;
+      return service.getCurrentTime(latitude, longitude)
+        .then((result: ITimeModel) => {
+          //check for error
+          if (!result) { return null; }
 
-                //else - set the label for the model
-                timeModel = result;
-                timeModel.label = card.locationName ?? card.address ?? card.name;
-                console.log("CURRENT TIME", {name: card.name, timeModel});
-                
-                return {
-                  text: `${timeModel.dayOfTheWeek}: ${timeModel.time}`,
-                  icon: env.logo.white,
-                  color: 'sky',
-                  refresh: 30
-                };
-              });
-          }
-          //else 
-          console.log("BADGE UPDATE", {name: card.name, timeModel});
-
+          //else - set the label for the model
+          timeModel = result;
+          timeModel.label = card.locationName ?? card.address ?? card.name;
+          console.log("CURRENT TIME", {name: card.name, timeModel});
+          
           return {
             text: `${timeModel.dayOfTheWeek}: ${timeModel.time}`,
             icon: env.logo.white,
-            color: 'lime',
-            refresh: 30
+            color: 'sky',
           };
-        }
-      };
+        });
+
+      // return {
+      //   dynamic: () => {
+      //     if (timeModel == null) {
+      //       const service = new TimeService(t);
+      //       const {latitude, longitude} = card.coordinates;
+      //       return service.getCurrentTime(latitude, longitude)
+      //         .then((result: ITimeModel) => {
+      //           //check for error
+      //           if (!result) { return null; }
+
+      //           //else - set the label for the model
+      //           timeModel = result;
+      //           timeModel.label = card.locationName ?? card.address ?? card.name;
+      //           console.log("CURRENT TIME", {name: card.name, timeModel});
+                
+      //           return {
+      //             text: `${timeModel.dayOfTheWeek}: ${timeModel.time}`,
+      //             icon: env.logo.white,
+      //             color: 'sky',
+      //             refresh: 30
+      //           };
+      //         });
+      //     }
+      //     //else 
+      //     console.log("BADGE UPDATE", {name: card.name, timeModel});
+
+      //     return {
+      //       text: `${timeModel.dayOfTheWeek}: ${timeModel.time}`,
+      //       icon: env.logo.white,
+      //       color: 'lime',
+      //       refresh: 30
+      //     };
+      //   }
+      // };
     }
     //else
     return null;
